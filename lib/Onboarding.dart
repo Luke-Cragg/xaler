8061c05cb9889609fa0cf74a617abb00d2978361
luke-cragg <lukecragg02@outlook.com>
2023-10-02 17:33:23 +0100

    Onboarding screen customised

--- lib/Onboarding.dart
+++ lib/Onboarding.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'Home.dart';
-import 'package:intro_slider/intro_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({Key? key}) : super(key: key);
@@ -11,194 +12,94 @@ class Onboarding extends StatefulWidget {
 }
 
 class onboardingPage extends State<Onboarding> {
-  final List<ContentConfig> ContentList = [];
+  //final List<ContentConfig> ContentList = [];
   final Color Purple = const Color(0xFFAC008F);
+  final _PageController = PageController();
+  bool isLastPage = false;
 
   @override
   void initState() {
     super.initState();
-
-    ContentList.add(
-      const ContentConfig(
-        title: "Hey, I'm Xaler",
-        description:
-            "Lets get to know each other! \n\nI have a few questions to ask you so I can get to know you better!",
-        backgroundColor: Color(0xFFAC008F),
-      ),
-    );
-    ContentList.add(
-      const ContentConfig(
-        title: "Question 1: ",
-        description: "How are you feeling today",
-        backgroundColor: Color(0xFFFF5F1F),
-      ),
-    );
   }
 
-  @override
-  Widget build(BuildContext) {
-    return IntroSlider(
-      key: UniqueKey(),
-      listContentConfig: ContentList,
-      onDonePress: () => Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => const Home()),
-      ),
-      curveScroll: Curves.bounceIn,
-      indicatorConfig: IndicatorConfig(
-        sizeIndicator: 10,
-        indicatorWidget: Container(
-          height: 10,
-          decoration: BoxDecoration(
-            color: Colors.white,
+  void dispose() {
+    _PageController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          padding: const EdgeInsets.only(bottom: 0),
+          child: PageView(
+            controller: _PageController,
+            onPageChanged: (index) {
+              setState(() => isLastPage = index == 2);
+            },
+            children: [
+              Container(
+                color: Colors.indigo,
+                child: const Center(child: Text('Page 1')),
+              ),
+              Container(
+                color: Colors.orange,
+                child: const Center(child: Text('Page 2')),
+              ),
+              Container(
+                color: Purple,
+                child: const Center(child: Text('Page 3')),
+              ),
+            ],
           ),
         ),
-        activeIndicatorWidget: Container(
-          height: 10,
-          width: 10,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(30)),
-        ),
-        spaceBetweenIndicator: 5,
-        typeIndicatorAnimation: TypeIndicatorAnimation.sliding,
-      ),
-    );
-  }
+        bottomSheet: isLastPage
+            ? TextButton(
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                  primary: Colors.white,
+                  backgroundColor: Colors.teal.shade700,
+                  minimumSize: const Size.fromHeight(60),
+                ),
+                onPressed: () async {
+                  // final prefs = await SharedPreferences.getInstance();
+                  // prefs.setBool('showHome', true);
+
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const Home()));
+                },
+                child: Text(
+                  'Lets Go!',
+                  style: GoogleFonts.quicksand(fontSize: 24),
+                ),
+              )
+            : Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                height: 60,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                          onPressed: () => _PageController.jumpToPage(2),
+                          child: Text('Skip',
+                              style: GoogleFonts.quicksand(fontSize: 18))),
+                      Center(
+                        child: SmoothPageIndicator(
+                          controller: _PageController,
+                          count: 3,
+                          effect: WormEffect(
+                              spacing: 16,
+                              dotColor: Colors.black38,
+                              activeDotColor: Colors.teal.shade700),
+                        ),
+                      ),
+                      TextButton(
+                          onPressed: () => _PageController.nextPage(
+                              duration: const Duration(milliseconds: 800),
+                              curve: Curves.easeOutCirc),
+                          child: Text('Next',
+                              style: GoogleFonts.quicksand(fontSize: 18))),
+                    ]),
+              ),
+      );
 }
-
-
-// class Onboarding extends StatefulWidget {
-//   const Onboarding({super.key});
-//   @override
-//   State<Onboarding> createState() => OnboardingPage();
-// }
-
-// class OnboardingPage extends State<Onboarding> {
-//   int currentPage = 0;
-//   final PageController _pageController = PageController(initialPage: 0);
-
-//   final List<Widget> _pages = [
-//     Container(
-//       color: Purple,
-//       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-//         Container(
-//           child: Column(
-//             children: [
-//               //alignment: Alignment.topCenter,
-//               Text(
-//                 "Hey I'm Xaler, \nLets get to know each other!",
-//                 style: GoogleFonts.quicksand(fontSize: 25, color: Colors.white),
-//                 textAlign: TextAlign.center,
-//               ),
-//               SizedBox(height: 100),
-//               Center(
-//                 child: Container(
-//                   child: Text(
-//                     "I'm your personal 24/7 mental health tracker and buddy. \n",
-//                     style: GoogleFonts.quicksand(
-//                         fontSize: 21, color: Colors.white),
-//                     textAlign: TextAlign.center,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ]),
-//     ),
-//     Container(
-//       color: Purple,
-//       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-//         Container(
-//           child: Column(
-//             children: [
-//               //alignment: Alignment.topCenter,
-//               Text(
-//                 ,
-//                 style: GoogleFonts.quicksand(fontSize: 25, color: Colors.white),
-//               ),
-//               SizedBox(height: 50),
-//               Center(
-//                 child: Container(
-//                   padding: const EdgeInsets.all(0.0),
-//                   child: Text(
-//                     "I'm Xaler, your personal 24/7 mental health tracker and buddy. I have a few questions to ask you so I can get to know you better!",
-//                     style: GoogleFonts.quicksand(
-//                         fontSize: 21, color: Colors.white),
-//                     textAlign: TextAlign.center,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ]),
-//     ),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: PageView(
-//           controller: _pageController,
-//           onPageChanged: (int page) {
-//             setState(() {
-//               currentPage = page;
-//             });
-//           },
-//           children: _pages,
-//         ),
-//         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-//         floatingActionButton: Stack(
-//           fit: StackFit.expand,
-//           children: [
-//             Positioned(
-//               left: 30,
-//               bottom: 20,
-//               child: FloatingActionButton(
-//                 heroTag: "Previous",
-//                 backgroundColor: currentPage > 0 ? Colors.blue : Colors.grey,
-//                 onPressed: currentPage > 0
-//                     ? () {
-//                         setState(() {
-//                           currentPage -= 1;
-//                         });
-//                         _pageController.previousPage(
-//                             duration: const Duration(milliseconds: 500),
-//                             curve: Curves.ease);
-//                       }
-//                     : null,
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//                 child: const Icon(Icons.arrow_left, size: 40),
-//               ),
-//             ),
-//             Positioned(
-//               right: 30,
-//               bottom: 20,
-//               child: FloatingActionButton(
-//                 heroTag: "next",
-//                 onPressed: currentPage < _pages.length - 1
-//                     ? () {
-//                         setState(() {
-//                           currentPage += 1;
-//                         });
-//                         _pageController.nextPage(
-//                             duration: const Duration(milliseconds: 500),
-//                             curve: Curves.ease);
-//                       }
-//                     : () {
-//                         Navigator.of(context).pushReplacement(
-//                             MaterialPageRoute(builder: (_) => const Home()));
-//                       },
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(10),
-//                 ),
-//                 child: const Icon(Icons.arrow_right, size: 40),
-//               ),
-//             )
-//           ],
-//         ));
-//   }
-// }
