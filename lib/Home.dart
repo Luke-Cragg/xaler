92c90349d3f8948431bdf352a329dac3238a4c30
Luke-Cragg <124095690+Luke-Cragg@users.noreply.github.com>
2023-10-05 14:40:26 +0100

    Home Page with Settings button and nav

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'main.dart';
+import 'settings.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,17 +14,25 @@ class HomePage extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("Welcome"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: IconButton(
+              icon: const Icon(
+                Icons.settings,
+                size: 35,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Settings()));
+              },
+            ),
+          ),
+        ],
       ),
-      body: ElevatedButton(
-        onPressed: () async {
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          prefs.remove('email');
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => MainStatefulWidget()));
-        },
-        child: Text("Sign out"),
-      ),
+      body: Text("Woo Home Page"),
     );
   }
 }
