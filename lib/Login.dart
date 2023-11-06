eac2ad5b9f5a22771fa9c496dc3a199d67f650e8
luke-cragg <lukecragg02@outlook.com>
2023-11-06 14:40:13 +0000

    Addition of login page seperate to main

---
+++ lib/Login.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future Signin() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //GetCurrentUserUID();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(Icons.leave_bags_at_home))
+        ],
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: Signin,
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,165 +5,210 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:xaler/Home.dart';
 import 'package:xaler/Onboarding.dart';
 import 'Navigation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'Login.dart';
+
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+// Container SignInButton(BuildContext context, bool isLogin, Function onTap) {
+//   return Container(
+//     height: 50,
+//     padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+//     child: ElevatedButton(
+//       onPressed: () {
+//         onTap();
+//       },
+//       child: const Text('Log In'),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   static const String _title = 'XALER';
+//   static const type = 1;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: _title,
+//       home: Scaffold(
+//         extendBodyBehindAppBar: true,
+//         appBar: AppBar(
+//           toolbarHeight: 120,
+//           title: Container(
+//             padding: const EdgeInsets.only(top: 40.0),
+//             child: Text(_title,
+//                 style:
+//                     GoogleFonts.quicksand(color: Colors.white, fontSize: 60.0)),
+//           ),
+//           elevation: 0,
+//           backgroundColor: Colors.transparent,
+//           centerTitle: true,
+//         ),
+//         body: const MainStatefulWidget(),
+//       ),
+//     );
+//   }
+// }
+
+// class MainStatefulWidget extends StatefulWidget {
+//   const MainStatefulWidget({Key? key}) : super(key: key);
+
+//   @override
+//   State<MainStatefulWidget> createState() => _MainStatefulWidget();
+// }
+
+// class _MainStatefulWidget extends State<MainStatefulWidget> {
+//   TextEditingController nameController = TextEditingController();
+//   TextEditingController passwordController = TextEditingController();
+
+//   Future<void> checkLoginStatus() async {
+//     SharedPreferences prefs = await SharedPreferences.getInstance();
+//     String? email = prefs.getString('email');
+//     String? password = prefs.getString('password');
+
+//     if (email != null && password != null) {
+//       Navigator.push(
+//           context, MaterialPageRoute(builder: (context) => const MyNav()));
+//     }
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     checkLoginStatus();
+//   }
+
+//   Future LogIn() async {
+//     await FirebaseAuth.instance.signInWithEmailAndPassword(
+//         email: nameController.text.trim(),
+//         password: passwordController.text.trim());
+//   }
+
+//   @override
+//   void dispose() {
+//     nameController.dispose();
+//     passwordController.dispose();
+//     super.dispose();
+//   }
+
+//   final Color backGround = const Color(0xFF003A6C);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//           width: double.infinity,
+//           decoration: BoxDecoration(
+//             color: backGround,
+//           ),
+//           child: ListView(
+//             padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+//             children: <Widget>[
+//               Container(
+//                 alignment: Alignment.centerLeft,
+//                 padding: const EdgeInsets.all(10),
+//                 child: Text(
+//                   'Log in',
+//                   style:
+//                       GoogleFonts.quicksand(fontSize: 30, color: Colors.white),
+//                 ),
+//               ),
+//               Container(
+//                 padding: const EdgeInsets.all(10),
+//                 child: TextField(
+//                     controller: nameController,
+//                     style: const TextStyle(color: Colors.black),
+//                     decoration: const InputDecoration(
+//                       border: OutlineInputBorder(),
+//                       filled: true,
+//                       fillColor: Colors.white,
+//                       labelText: 'UserName',
+//                     )),
+//               ),
+//               Container(
+//                 padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+//                 child: TextField(
+//                     obscureText: true,
+//                     controller: passwordController,
+//                     decoration: const InputDecoration(
+//                       hintStyle: TextStyle(color: Colors.white),
+//                       border: OutlineInputBorder(),
+//                       filled: true,
+//                       fillColor: Colors.white,
+//                       labelText: 'Password',
+//                     )),
+//               ),
+//               TextButton(
+//                 onPressed: () {},
+//                 child: const Text('Forgot Password'),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () async {
+//                   String email = nameController.text;
+//                   String password = passwordController.text;
+//                   SharedPreferences prefs =
+//                       await SharedPreferences.getInstance();
+//                   prefs.setString('email', email);
+//                   prefs.setString('password', password);
+
+//                   // Navigator.pushReplacement(context,
+//                   //     MaterialPageRoute(builder: (context) => MyNav()));
+//                   LogIn();
+//                 },
+//                 child: Text("Sign in"),
+//               ),
+//             ],
+//           )),
+//     );
+//   }
+// }
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
-}
-
-Container SignInButton(BuildContext context, bool isLogin, Function onTap) {
-  return Container(
-    height: 50,
-    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-    child: ElevatedButton(
-      onPressed: () {
-        onTap();
-      },
-      child: const Text('Log In'),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'XALER';
-  static const type = 1;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: _title,
-      home: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          toolbarHeight: 120,
-          title: Container(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Text(_title,
-                style:
-                    GoogleFonts.quicksand(color: Colors.white, fontSize: 60.0)),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-        ),
-        body: const MainStatefulWidget(),
-      ),
+      home: MainPage(),
     );
   }
 }
 
-class MainStatefulWidget extends StatefulWidget {
-  const MainStatefulWidget({Key? key}) : super(key: key);
-
+class MainPage extends StatelessWidget {
   @override
-  State<MainStatefulWidget> createState() => _MainStatefulWidget();
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                User? user = snapshot.data;
+                if (user == null) {
+                  return Login();
+                } else {
+                  return MyNav();
+                }
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
+      );
 }
 
-class _MainStatefulWidget extends State<MainStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  Future<void> checkLoginStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? email = prefs.getString('email');
-    String? password = prefs.getString('password');
-
-    if (email != null && password != null) {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => const MyNav()));
-    }
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-    checkLoginStatus();
-  }
-
-  Future LogIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: nameController.text.trim(),
-        password: passwordController.text.trim());
-  }
-
-  @override
-  void dispose() {
-    nameController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
-  final Color backGround = const Color(0xFF003A6C);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: backGround,
-          ),
-          child: ListView(
-            padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
-            children: <Widget>[
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'Log in',
-                  style:
-                      GoogleFonts.quicksand(fontSize: 30, color: Colors.white),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                    controller: nameController,
-                    style: const TextStyle(color: Colors.black),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: 'UserName',
-                    )),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: 'Password',
-                    )),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text('Forgot Password'),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  String email = nameController.text;
-                  String password = passwordController.text;
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  prefs.setString('email', email);
-                  prefs.setString('password', password);
-
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => MyNav()));
-                },
-                child: Text("Sign in"),
-              ),
-            ],
-          )),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: Text("Hello World"),
+      );
 }
