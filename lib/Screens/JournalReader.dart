e808f9d9b113e467b0ab94568d4413c5d7437b00
Luke-Cragg <124095690+Luke-Cragg@users.noreply.github.com>
2023-12-19 10:52:23 +0000

    Journal Reader page

--- lib/Screens/JournalReader.dart
+++ lib/Screens/JournalReader.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class JournalReader extends StatefulWidget {
   JournalReader(this.doc, {super.key});
@@ -9,20 +10,47 @@ class JournalReader extends StatefulWidget {
 }
 
 class _JournalReaderState extends State<JournalReader> {
+  final Color backColor = const Color(0xFF38434E);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(widget.doc["entry_title"]),
-            Text(widget.doc["creation_date"]),
-            SizedBox(height: 28.0),
-            Text(widget.doc["entry_content"]),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(color: Colors.blueGrey),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.doc["entry_title"],
+                style: GoogleFonts.quicksand(
+                    fontSize: 26,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                widget.doc["creation_date"],
+                style: GoogleFonts.quicksand(fontSize: 22, color: Colors.white),
+              ),
+              SizedBox(height: 28.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  widget.doc["entry_content"],
+                  style:
+                      GoogleFonts.quicksand(fontSize: 22, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
