1c0bd8bd5e1c13e3a4fffe9fb8775ba1201f8479
luke-cragg <lukecragg02@outlook.com>
2024-03-21 16:32:55 +0000

    Curved Bottom Navigation bar addition

--- lib/Navigation.dart
+++ lib/Navigation.dart
@@ -1,11 +1,14 @@
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'Home.dart';
 import 'Journal.dart';
 import 'mindfulness.dart';
 import 'Resources.dart';
 
-var bottomNavKey = GlobalKey<State<BottomNavigationBar>>();
 int pageIndex = 0;
+GlobalKey<CurvedNavigationBarState> bottomNavKey = GlobalKey();
 
 class MyNav extends StatefulWidget {
   const MyNav({super.key});
@@ -15,40 +18,92 @@ class MyNav extends StatefulWidget {
 
 class _MyNavState extends State<MyNav> {
   final Color BackGrey = Color(0xFF3A3A3A);
+  bool resourceVisited = false;
+
   List<Widget> PageList = <Widget>[
     Home(),
     Mindfulness(),
     Journal(),
     Resources(),
   ];
+  void updateHomePage(bool completed) {
+    if (completed) {
+      setState(() {
+        pageIndex = 0;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageList[pageIndex],
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: CurvedNavigationBar(
         key: bottomNavKey,
-        elevation: 0,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: BackGrey,
-        selectedItemColor: Color(0XFF1E90FF),
-        unselectedItemColor: Color(0XFFFFFFFF),
-        currentIndex: pageIndex,
         onTap: (index) {
           setState(() {
             pageIndex = index;
           });
         },
+        color: BackGrey,
+        backgroundColor: Color(0XFF1E90FF),
+        buttonBackgroundColor: BackGrey,
+        index: pageIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.sunny), label: 'Mindfulness'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.message_outlined), label: 'Vent'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.format_list_bulleted_outlined),
-              label: 'Resources')
+          CurvedNavigationBarItem(
+              child: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              label: 'Home',
+              labelStyle: TextStyle(color: Colors.white)),
+          CurvedNavigationBarItem(
+              child: Icon(
+                Icons.sunny,
+                color: Colors.white,
+              ),
+              label: 'Mindfulness',
+              labelStyle: TextStyle(color: Colors.white)),
+          CurvedNavigationBarItem(
+              child: Icon(
+                Icons.message_outlined,
+                color: Colors.white,
+              ),
+              label: 'Journal',
+              labelStyle: TextStyle(color: Colors.white)),
+          CurvedNavigationBarItem(
+              child: Icon(
+                Icons.format_list_bulleted_outlined,
+                color: Colors.white,
+              ),
+              label: 'Resources',
+              labelStyle: TextStyle(color: Colors.white)),
         ],
       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   key: bottomNavKey,
+      //   elevation: 0,
+      //   type: BottomNavigationBarType.fixed,
+      //   backgroundColor: BackGrey,
+      //   selectedItemColor: Color(0XFF1E90FF),
+      //   unselectedItemColor: Color(0XFFFFFFFF),
+      //   currentIndex: pageIndex,
+      //   onTap: (index) {
+      //     setState(() {
+      //       pageIndex = index;
+      //     });
+      //   },
+      //   items: const [
+      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.sunny), label: 'Mindfulness'),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.message_outlined), label: 'Journal'),
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.format_list_bulleted_outlined),
+      //         label: 'Resources')
+      //   ],
+      // ),
     );
   }
 }
