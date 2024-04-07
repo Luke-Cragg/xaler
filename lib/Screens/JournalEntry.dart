63d0b64e360cd9385dde33f181bb37e0ec0faea9
luke-cragg <lukecragg02@outlook.com>
2024-04-07 13:51:37 +0100

    Code cleanup

--- lib/Screens/JournalEntry.dart
+++ lib/Screens/JournalEntry.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../Journal.dart';
 import 'package:intl/intl.dart';
 
 class journalEntry extends StatefulWidget {
@@ -18,7 +17,6 @@ class _journalEntryState extends State<journalEntry> {
   String date = DateFormat('d/M/y').format(DateTime.now());
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   String UserId = '';
 
 //Gets the current signed in users firebase UID. This allows for data to be created
@@ -33,9 +31,10 @@ class _journalEntryState extends State<journalEntry> {
 
   @override
   Widget build(BuildContext context) {
-    final Color backColor = const Color(0xFF38434E);
+    const Color backColor = Color(0xFF38434E);
     GetCurrentUserUID();
     return Scaffold(
+      //resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(
           "Journal Entry",
@@ -46,59 +45,64 @@ class _journalEntryState extends State<journalEntry> {
         backgroundColor: backColor,
       ),
       body: Container(
-        decoration: BoxDecoration(color: backColor),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                controller: _titleController,
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Enter your journal title",
-                    hintStyle: TextStyle(
-                        fontSize: 20, color: Color.fromARGB(96, 255, 255, 255)),
-                    fillColor: Colors.white),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                date,
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-              const SizedBox(height: 20),
-              const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Hints:\n• How was today?\n• What was the highlight of your day?\n• What was not so great today?\n• Use this area for whatever you need to vent\n• This is your private safe space to let it out!",
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  )),
-              SizedBox(height: 10),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Color.fromARGB(96, 255, 255, 255), width: 1)),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                    controller: _contentController,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Enter your journal entry",
-                        hintStyle: TextStyle(
-                            color: Color.fromARGB(96, 255, 255, 255))),
+        decoration: const BoxDecoration(color: backColor),
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                  controller: _titleController,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Enter your journal title",
+                      hintStyle: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromARGB(96, 255, 255, 255)),
+                      fillColor: Colors.white),
+                ),
+                const SizedBox(height: 8.0),
+                Text(
+                  date,
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                const SizedBox(height: 20),
+                const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Hints:\n• How was today?\n• What was the highlight of your day?\n• What was not so great today?\n• Use this area for whatever you need to vent\n• This is your private safe space to let it out!",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    )),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color.fromARGB(96, 255, 255, 255),
+                          width: 1)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                      controller: _contentController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Enter your journal entry",
+                          hintStyle: TextStyle(
+                              color: Color.fromARGB(96, 255, 255, 255))),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0XFF1E90FF),
+        backgroundColor: const Color(0XFF1E90FF),
         onPressed: () async {
           FirebaseFirestore.instance
               .collection("Journal")
@@ -113,7 +117,7 @@ class _journalEntryState extends State<journalEntry> {
             Navigator.pop(context);
           }).catchError((error) => print("Failed to add new note: $error"));
         },
-        child: Icon(Icons.save),
+        child: const Icon(Icons.save),
       ),
     );
   }
